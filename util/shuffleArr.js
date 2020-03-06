c9895752a9693fe269923a24ae19ccf060741409
H37kouya <h37kouya@gmail.com>
2020-03-06 21:37:57 +0900

    シャッフル関数の分離

---
+++ util/shuffleArr.js
@@ -0,0 +1,11 @@
+// 配列のシャッフル関数
+export function shuffleArr(array) {
+  for (let i = array.length - 2; i > 0; i--) {
+    const r = Math.floor(Math.random() * (i + 1))
+    const tmp = array[i]
+    array[i] = array[r]
+    array[r] = tmp
+  }
+
+  return array.reverse()
+}
