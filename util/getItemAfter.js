c273b9d8fc383eca12ab388b77152a9002ff6478
H37kouya <h37kouya@gmail.com>
2020-03-07 19:13:06 +0900

    コメントアウトの追加

--- util/getItemAfter.js
+++ util/getItemAfter.js
@@ -1,3 +1,4 @@
+// 一つ後の配列を取得、さらにidも注入
 export function getItemAfter(arr, count) {
   let item
   if (arr[count + 1]) {
--- util/getItemBefore.js
+++ util/getItemBefore.js
@@ -1,3 +1,4 @@
+// 一つ前の配列を取得、さらにidも注入
 export function getItemBefore(arr, count) {
   let item
   if (arr[count - 1]) {
