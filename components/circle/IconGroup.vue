0269f09769264ae993450d6e25c0adfc300c6173
H37kouya <h37kouya@gmail.com>
2020-03-08 00:01:40 +0900

    lineのアイコン追加

--- components/circle/IconGroup.vue
+++ components/circle/IconGroup.vue
@@ -13,6 +13,8 @@
 
       <facebook-icon v-else-if="isFacebook(key)" :key="key" :href="item.url" />
 
+      <line-icon v-else-if="isLine(key)" :key="key" :href="item.url" />
+
       <v-btn v-else :key="key" :href="item.url" class="mr-2 mb-4">
         {{ key }}{{ item.name }}
       </v-btn>
@@ -24,6 +26,7 @@
 import FacebookIcon from '@/components/util/FacebookIcon'
 import GithubIcon from '@/components/util/GithubIcon'
 import InstagramIcon from '@/components/util/InstagramIcon'
+import LineIcon from '@/components/util/LineIcon'
 import TwitterIcon from '@/components/util/TwitterIcon'
 
 export default {
@@ -31,6 +34,7 @@ export default {
     FacebookIcon,
     GithubIcon,
     InstagramIcon,
+    LineIcon,
     TwitterIcon
   },
 
@@ -42,20 +46,24 @@ export default {
   },
 
   methods: {
-    isTwitter(key) {
-      return key.toLowerCase() === 'twitter'
-    },
-
-    isInstagram(key) {
-      return key.toLowerCase() === 'instagram'
-    },
-
     isFacebook(key) {
       return key.toLowerCase() === 'facebook'
     },
 
     isGithub(key) {
       return key.toLowerCase() === 'github'
+    },
+
+    isInstagram(key) {
+      return key.toLowerCase() === 'instagram'
+    },
+
+    isLine(key) {
+      return key.toLowerCase() === 'line'
+    },
+
+    isTwitter(key) {
+      return key.toLowerCase() === 'twitter'
     }
   }
 }
