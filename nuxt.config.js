0f8ee24cffc3cb4e581acfcbd452e02b0335b8de
h37kouya <h37kouya@gmail.com>
2020-03-12 14:54:51 +0900

    サイトマップの作製

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 import colors from 'vuetify/es5/util/colors'
-// import Firebase from 'firebase'
+import Firebase from 'firebase'
 import 'firebase/firestore'
 require('dotenv').config()
 
@@ -173,34 +173,39 @@ export default {
   },
 
   sitemap: {
-    hostname: 'https://uu-circle20.firebaseapp.com'
-    // async routes() {
-    //   // ここで動的ページの出力をする
-    //   // firestoreの準備
-    //   const config = {
-    //     apiKey: process.env.FIREBASE_API_KEY,
-    //     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    //     databaseURL: process.env.FIREBASE_DB_URL,
-    //     projectId: process.env.FIREBASE_PROJECTID,
-    //     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    //     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    //     appId: process.env.FIREBASE_APP_ID,
-    //     measurementId: process.env.FIREBASE_MEASUREMENT_ID
-    //   }
-    //   const firebaseApp = Firebase.initializeApp(config)
-    //   const firestore = firebaseApp.firestore()
-    //   firestore.settings({ timestampsInSnapshots: true })
+    hostname: 'https://uu-circle20.firebaseapp.com',
 
-    //   // firestoreからDataの回収
-    //   const collection = firestore.collection('circles')
-    //   const docs = await collection.get()
+    async routes() {
+      // ここで動的ページの出力をする
+      // firestoreの準備
+      const config = {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DB_URL,
+        projectId: process.env.FIREBASE_PROJECTID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+      }
+      const firebaseApp = Firebase.initializeApp(config)
+      const firestore = firebaseApp.firestore()
+      firestore.settings({ timestampsInSnapshots: true })
+      // firestoreからDataの回収
+      const collection = firestore.collection('circles')
+      const docs = await collection.get()
 
-    //   console.log(docs)
+      // 戻り値の生成
+      const urls = []
+      const docsLen = docs.docs.length
+      for (let i = 0; i < docsLen; i++) {
+        const _doc = docs.docs[i]
+        const url = `circles/${_doc.id}`
+        urls.push(url)
+      }
 
-    //   const urls = []
-
-    //   return urls
-    // }
+      return urls
+    }
   },
 
   googleAnalytics: {
