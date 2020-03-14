d88cf107a9bc0f7c0c0d173ef454785b36f2b475
H37kouya <h37kouya@gmail.com>
2020-03-14 21:16:28 +0900

    デザインの追加、修正

--- components/layouts/MainBackground.vue
+++ components/layouts/MainBackground.vue
@@ -11,12 +11,12 @@
     <div class="main-background-content-container">
       <v-row style="width: 320px">
         <v-col cols="4" sm="4" class="d-flex justify-end">
-          <p style="width: 60px">
-            <img src="/icon.png" width="60px" />
+          <p style="width: 60px; padding-top: 16px">
+            <img src="/icon.png" width="60px" style="border-radius: 10px" />
           </p>
         </v-col>
 
-        <v-col cols="7" sm="8" md="8">
+        <v-col cols="8" sm="8" md="8">
           <div class="main-background-content d-flex flex-column">
             <span>宇都宮大学</span>
             <span>Utsunomiya</span>
@@ -27,16 +27,16 @@
     </div>
 
     <v-row class="main-background-bottom">
-      <v-col cols="3" class="main-background-bottom-l">
+      <v-col cols="2" sm="3" class="main-background-bottom-l">
         <div class="text-center">
           <v-icon color="#fff">mdi-information-variant</v-icon>
-          <p class="mb-0" style="font-size: 14px">重要な<br />お知らせ</p>
+          <p class="mb-0" style="font-size: 10px">重要な<br />お知らせ</p>
         </div>
       </v-col>
 
-      <v-col cols="9" class="main-background-bottom-r">
+      <v-col cols="10" sm="9" class="main-background-bottom-r">
         <p class="mb-0 text-center">
-          新型コロナウイルス感染症によって新歓日程に変更が生じる可能性がございます
+          新型コロナによって新歓日程に変更が生じる可能性がございます
         </p>
       </v-col>
     </v-row>
