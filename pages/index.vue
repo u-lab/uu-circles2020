70485416e99b61e09ce5d1396b7f527960a264de
H37kouya <h37kouya@gmail.com>
2020-03-19 23:17:40 +0900

    掲載団体数の追加

--- pages/index.vue
+++ pages/index.vue
@@ -83,6 +83,14 @@
         </v-btn>
       </div>
     </template>
+
+    <div class="d-flex justify-center my-4">
+      <div class="intro-background pa-6">
+        <p class="mb-0 text-center">
+          掲載団体数: <span style="font-size: 24px">{{ circles.length }}</span>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
