a26d368d2e59a3139c9a375821f6503974d507ab
H37kouya <h37kouya@gmail.com>
2020-03-20 11:50:45 +0900

    class="background"を<v-container>に変更

--- pages/about/company.vue
+++ pages/about/company.vue
@@ -1,39 +1,41 @@
 <template>
-  <v-row class="background">
-    <v-col cols="12">
-      <about-heading :num="1" class="about-company-list">
-        <span class="haifun">協賛企業・支援者一覧</span>
-      </about-heading>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <about-heading :num="1" class="about-company-list">
+          <span class="haifun">協賛企業・支援者一覧</span>
+        </about-heading>
 
-      <div class="d-flex justify-center">
-        <div class="intro-background mt-2 pa-4">
-          このサイトは協賛してくれている方々によって支えられています。
+        <div class="d-flex justify-center">
+          <div class="intro-background mt-2 pa-4">
+            このサイトは協賛してくれている方々によって支えられています。
+          </div>
         </div>
-      </div>
-    </v-col>
+      </v-col>
 
-    <v-col cols="12">
-      <div>
-        <p class="text-center">泉工業(株)</p>
-        <p class="text-center">伊勢元酒店</p>
-        <p class="text-center">お米ひろばさとう</p>
-        <p class="text-center">Future Rise</p>
-        <p class="text-center">
-          <small>※五十音順</small>
-        </p>
-      </div>
-
-      <div class="d-flex justify-center">
-        <div style="width: 320px">
-          <v-img src="/kyousan.jpg" alt="協賛募集" width="320" />
+      <v-col cols="12">
+        <div>
+          <p class="text-center">泉工業(株)</p>
+          <p class="text-center">伊勢元酒店</p>
+          <p class="text-center">お米ひろばさとう</p>
+          <p class="text-center">Future Rise</p>
+          <p class="text-center">
+            <small>※五十音順</small>
+          </p>
         </div>
-      </div>
-    </v-col>
 
-    <v-col cols="12" class="d-flex justify-center">
-      <circle-list-btn />
-    </v-col>
-  </v-row>
+        <div class="d-flex justify-center">
+          <div style="width: 320px">
+            <v-img src="/kyousan.jpg" alt="協賛募集" width="320" />
+          </div>
+        </div>
+      </v-col>
+
+      <v-col cols="12" class="d-flex justify-center">
+        <circle-list-btn />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
