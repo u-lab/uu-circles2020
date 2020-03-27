2636c930cf43101945715489f8d649445074d0a5
H37kouya <h37kouya@gmail.com>
2020-03-28 00:48:14 +0900

    製作系合同新歓説明会の記事を追加

---
+++ pages/events/0409-mono-newjoy.vue
@@ -0,0 +1,186 @@
+<template>
+  <v-container class="gosikku">
+    <about-heading :num="1" class="about-company-list">
+      <span class="haifun">製作系合同新歓説明会</span>
+    </about-heading>
+
+    <div class="py-4">
+      <p class="text-center">
+        新入生に宇都宮大学で活動するものづくりサークルのことを知ってもらえれば嬉しいです
+      </p>
+      <p class="text-center">
+        webライブ配信、アーカイブ動画の投稿も予定しているので、参加しないでアーカイブ動画だけを見る感じでも大歓迎です！
+      </p>
+
+      <div class="d-flex justify-center">
+        <v-img
+          :src="mainImg"
+          alt="製作系合同新歓説明会ロゴ"
+          max-width="400px"
+        />
+      </div>
+
+      <div>
+        <about-heading :num="2" class="text-center">
+          参加団体
+        </about-heading>
+        <p class="text-center">
+          U-lab Cactus ものづくり研究会 UCG...etc
+        </p>
+      </div>
+
+      <div>
+        <about-heading :num="2" class="text-center">
+          開催日時
+        </about-heading>
+
+        <p class="text-center">
+          <span class="zenkaku">4月9日</span>18:00-20:00
+        </p>
+      </div>
+
+      <div>
+        <about-heading :num="2" class="text-center">
+          開催場所
+        </about-heading>
+        <p class="text-center">
+          プラザ２階
+        </p>
+      </div>
+
+      <div>
+        <about-heading :num="2" class="text-center">
+          概要
+        </about-heading>
+        <p class="text-center">
+          各サークル、団体がどのような目的でどんな活動をしているのか聞くことができます。
+        </p>
+
+        <div class="d-flex justify-center">
+          <table>
+            <tr>
+              <th class="pr-1">18:00-</th>
+              <td>各サークル団体紹介</td>
+            </tr>
+            <tr>
+              <th class="pr-1">19:30-</th>
+              <td>お悩み相談会</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+
+    <p class="text-center" style="font-size: 80%">
+      <span class="kome">
+        新型コロナウイルスに対して運営側はマスク、アルコール消毒などの対策に努めます。また新型コロナウイルスを考慮しご飯会などは行いません
+      </span>
+    </p>
+
+    <div>
+      <about-heading :num="2" class="text-center">
+        参加団体ビラ
+      </about-heading>
+
+      <div>
+        <p class="text-center">
+          <nuxt-link to="/circles/u-lab">U-lab</nuxt-link>
+        </p>
+        <p class="text-center">
+          <nuxt-link to="/circles/cactus">Cactus</nuxt-link>
+        </p>
+        <p class="text-center">
+          <nuxt-link to="/circles/mono">ものづくり研究会</nuxt-link>
+        </p>
+        <p class="text-center"><nuxt-link to="/circles/ucg">UCG</nuxt-link></p>
+      </div>
+    </div>
+
+    <v-col cols="12" class="d-flex justify-center">
+      <circle-list-btn />
+    </v-col>
+  </v-container>
+</template>
+
+<script>
+import AboutHeading from '@/components/about/AboutHeading'
+import CircleListBtn from '@/components/util/CircleListBtn'
+import MainImg from '@/assets/img/events/0409-mono-newjoy1.jpg'
+
+export default {
+  components: {
+    AboutHeading,
+    CircleListBtn
+  },
+
+  computed: {
+    mainImg() {
+      return MainImg
+    }
+  },
+
+  head() {
+    return {
+      title: '製作系合同新歓説明会',
+      meta: [
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: '製作系合同新歓説明会'
+        },
+        { hid: 'og:type', property: 'og:type', content: 'article' }
+      ],
+      __dangerouslyDisableSanitizers: ['script'],
+      script: [
+        {
+          innerHTML: `{
+            "@context": "http://schema.org",
+            "@type": "BreadcrumbList",
+            "name": "宇都宮大学の部活動・サークル・学生団体のビラ一覧",
+            "url": "https://uu-circle20.firebaseapp.com",
+            "itemListElement": [
+              {
+                "@type": "ListItem",
+                "position": 1,
+                "item": {
+                  "@id": "https://uu-circle20.firebaseapp.com",
+                  "name": "ビラ一覧"
+                }
+              },
+              {
+                "@type": "ListItem",
+                "position": 2,
+                "item": {
+                  "@id": "https://uu-circle20.firebaseapp.com/events/0409-mono-newjoy",
+                  "name": "製作系合同新歓説明会"
+                }
+              }
+            ]
+          }`,
+          type: 'application/ld+json'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.gosikku {
+  font-family: 'Hiragino Sans W3', 'Hiragino Kaku Gothic ProN',
+    'ヒラギノ角ゴ ProN W3', 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', 'MS PGothic',
+    sans-serif;
+}
+
+.zenkaku {
+  &::after {
+    content: '  ';
+  }
+}
+
+.kome {
+  &::before {
+    content: '※';
+  }
+}
+</style>
