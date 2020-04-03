9482b6bbe7708a2e62e9fec0d8ff00883e586620
H37kouya <h37kouya@gmail.com>
2020-04-03 10:50:58 +0900

    helper関数に置き換え

--- util/circles/fixedCircle.js
+++ util/circles/fixedCircle.js
@@ -1,14 +1,9 @@
+import { swapArr } from '@/util/arrayHelper'
+
 export function fixedCircleById(circles, circleId, fixNum = 0) {
   for (let i = 0; i < circles.length; i++) {
     if (circles[i].id === circleId) {
       return swapArr(circles, fixNum, i)
     }
   }
-}
-
-function swapArr(arr, num1, num2) {
-  const tmp = arr[num1]
-  arr[num1] = arr[num2]
-  arr[num2] = tmp
-  return arr
 }
