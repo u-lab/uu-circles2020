0cd12fbe0455d05a33b90932c3a16284fa913de8
H37kouya <h37kouya@gmail.com>
2020-04-03 20:47:26 +0900

    #38 アトミックデザイン、ボタン

---
+++ components/atoms/icons/TwitterIcon.vue
@@ -0,0 +1,42 @@
+<template>
+  <base-icon :href="href" :src="src" :width="width" />
+</template>
+
+<script>
+import BaseIcon from '@/components/atoms/icons'
+
+export default {
+  components: {
+    BaseIcon
+  },
+
+  props: {
+    href: {
+      type: String,
+      required: true
+    },
+
+    src: {
+      type: String,
+      default: null,
+      required: false
+    },
+
+    width: {
+      type: [Number, String],
+      default: 50,
+      required: false
+    }
+  },
+
+  computed: {
+    baseIconSrc() {
+      return '/twitter.png'
+    },
+
+    iconSrc() {
+      return this.src || baseIconSrc
+    }
+  }
+}
+</script>
