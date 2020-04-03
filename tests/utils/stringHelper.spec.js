2f968406b3d27c32dd5e8eefc5c4a6167cb6f110
H37kouya <h37kouya@gmail.com>
2020-04-03 10:28:57 +0900

    kanaToHiraのテストを追加

---
+++ tests/utils/stringHelper.spec.js
@@ -0,0 +1,20 @@
+import * as stringHelpers from '@/util/stringHelper'
+
+describe('util/stringHelper', () => {
+  describe('kanaToHira', () => {
+    it('カタカナをひらがなに変換できる(正常系)', () => {
+      const resultStr = stringHelpers.kanaToHira('アイウエオ')
+      expect(resultStr).toBe('あいうえお')
+    })
+
+    it('ひらがなはひらがなのままである(正常系)', () => {
+      const resultStr = stringHelpers.kanaToHira('あいうえお')
+      expect(resultStr).toBe('あいうえお')
+    })
+
+    it('英数カナまじりで、カタカナのみひらがなに変換できる(正常系)', () => {
+      const resultStr = stringHelpers.kanaToHira('アaｂcイウエオ1２')
+      expect(resultStr).toBe('あaｂcいうえお1２')
+    })
+  })
+})
