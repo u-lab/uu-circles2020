3618cc067e5503778788189097c7430c77b34b1d
H37kouya <h37kouya@gmail.com>
2020-04-10 10:57:12 +0900

    interviewsへのリンクとデザイン追加

--- components/layouts/UuFooter.vue
+++ components/layouts/UuFooter.vue
@@ -1,13 +1,8 @@
 <template>
   <v-footer v-once class="d-flex justify-center flex-column">
+    <nuxt-link to="/interviews" class="mb-2">インタビュー記事一覧</nuxt-link>
     <nuxt-link to="/about/creater" class="mb-2">このサイトについて</nuxt-link>
     <nuxt-link to="/about/company" class="mb-4">協賛企業・支援者一覧</nuxt-link>
     <small class="mb-2">2020 &copy; 宇都宮大学情報デザインサークル U-lab</small>
   </v-footer>
 </template>
-
-<script>
-export default {}
-</script>
-
-<style></style>
