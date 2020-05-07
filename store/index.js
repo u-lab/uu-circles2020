4630916b1e0fc4e567b3c553e79a67d1d9900598
H37kouya <h37kouya@gmail.com>
2020-05-07 14:42:07 +0900

    保存

--- store/index.js
+++ store/index.js
@@ -1,96 +1,35 @@
-import clonedeep from 'lodash.clonedeep'
 import { shuffleArr } from '@/util/arrayHelper'
 import { fixedCircleById } from '@/util/circles/fixedCircle'
-import {
-  fetchCircleImageAll,
-  fetchCircleImageById,
-  fetchCirclesByFireStore
-} from '@/util/circles/fetchCircle'
 import circlesJson from '@/assets/json/circles.json'
 
 // state
 export const state = () => ({
-  circles: null,
-  cmpCircleNum: 0
+  circles: null
 })
 
 // getters
 export const getters = {
   circles: (state) => state.circles,
-  cmpCircleNum: (state) => state.cmpCircleNum,
-  check: (state) => state.circles !== null,
-  isCmp: (state) => state.cmpCircleNum === state.circles.length
+  check: (state) => state.circles !== null
 }
 
 // mutations
 export const mutations = {
   SET_CIRCLES(state, circles) {
     state.circles = circles
-  },
-
-  SET_CIRCLES_IMAGE(state, num, value) {
-    state.circles[num].image = value
-  },
-
-  UPDATE_CIRCLE_SUBIMAGES(state, obj) {
-    state.circles[obj.num].subImage = obj.urls
-  },
-
-  UPDATE_CIRCLE(state, { circles, num }) {
-    state.circles[num] = circles[0]
-  },
-
-  UPDATE_CIRCLES(state, { circles, startNum, endNum }) {
-    for (let i = 0, num = startNum; num < endNum; i++, num++) {
-      state.circles[num] = circles[i]
-    }
-  },
-
-  UPDATE_CMPCIRCIRCLENUM(state, num) {
-    state.cmpCircleNum = num
   }
 }
 
 // actions
 export const actions = {
-  async fetchCircles({ commit, getters }) {
+  fetchCircles({ commit, getters }) {
     if (!getters.check) {
-      const circles =
-        circlesJson || (await fetchCirclesByFireStore(this.$fireStore))
-
-      if (circlesJson) {
-        commit('UPDATE_CMPCIRCIRCLENUM', circles.length)
-      }
-
+      const circles = circlesJson
       // サークルのシャッフル
       shuffleArr(circles)
       // サークルの上位表示の固定
       fixedCircleById(circles, 'u-lab', 0)
       commit('SET_CIRCLES', circles)
     }
-  },
-
-  async fetchCircleImageAll({ commit, getters }, { startNum, endNum }) {
-    if (!getters.isCmp) {
-      const storageRef = this.$fireStorage.ref()
-
-      let circles = clonedeep(getters.circles.slice(startNum, endNum))
-      circles = await fetchCircleImageAll(circles, storageRef)
-      commit('UPDATE_CIRCLES', { circles, startNum, endNum })
-      commit('UPDATE_CMPCIRCIRCLENUM', endNum)
-    }
-  },
-
-  async fetchSubImage({ commit, getters }, { circleId }) {
-    let circles = getters.circles
-
-    // 画像のURLの取得(サブ画像付き)
-    circles = await fetchCircleImageById(
-      circles,
-      circleId,
-      this.$fireStorage.ref()
-    )
-
-    commit('SET_CIRCLES', circles)
   }
 }
