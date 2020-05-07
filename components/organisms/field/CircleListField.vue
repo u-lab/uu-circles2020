14153263315619e2d514f89c2a1a59308e62e8cb
H37kouya <h37kouya@gmail.com>
2020-05-07 14:59:23 +0900

    保存

---
+++ components/organisms/field/CircleListField.vue
@@ -0,0 +1,136 @@
+<template>
+  <!-- 検索 -->
+  <div>
+    <div class="d-flex justify-center align-center my-2">
+      <div class="px-1">
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn dark v-on="on">
+              系統検索
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item @click="clearFilterCircle()">
+              <v-list-item-title v-text="'すべて'" />
+            </v-list-item>
+
+            <v-list-item
+              v-for="typeList in typeListForSearch"
+              :key="`type-${typeList.type}`"
+              @click="computedCircleByType(typeList.type)"
+            >
+              <v-list-item-title v-text="typeList.name" />
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+
+      <div class="px-1">
+        <v-text-field
+          v-model="searchBox"
+          append-icon="mdi-magnify"
+          label="検索"
+          solo
+          hide-details
+          @input="computedCircleBySearchBox()"
+        />
+      </div>
+    </div>
+
+    <gacha-banner-field />
+
+    <circle-image-list
+      v-if="filterCirlce && filterCirlce.length > 0"
+      :circles="filterCirlce"
+    />
+
+    <template v-else>
+      <search-result-empty-text />
+
+      <div class="d-flex justify-center mb-4">
+        <v-btn dark @click="clearFilterCircle()">
+          検索リセット
+        </v-btn>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import { filterCircle } from '@/util/search'
+const GachaBannerField = () =>
+  import('@/components/molecules/field/GachaBannerField')
+const SearchResultEmptyText = () =>
+  import('@/components/atoms/text/SearchResultEmptyText')
+const CircleImageList = () =>
+  import('@/components/organisms/list/CircleImageList')
+
+const TYPE_LIST = [
+  { name: '運動系', type: 'sports' },
+  { name: '音楽系', type: 'music' },
+  { name: '文化系', type: 'culture' },
+  { name: '製作系', type: 'mono' },
+  { name: '農業系', type: 'agri' }
+]
+
+export default {
+  components: {
+    CircleImageList,
+    GachaBannerField,
+    SearchResultEmptyText
+  },
+
+  props: {
+    circles: {
+      type: Array,
+      default: undefined
+    }
+  },
+
+  data() {
+    return {
+      filterCirlce: this.circles,
+      searchBox: ''
+    }
+  },
+
+  computed: {
+    loadingAnimeNum() {
+      return 2
+    },
+
+    getLoadingKeyName() {
+      return (num) => {
+        return `loading-${num}`
+      }
+    },
+
+    typeListForSearch: () => TYPE_LIST
+  },
+
+  created() {
+    this.clearFilterCircle()
+  },
+
+  methods: {
+    clearFilterCircle() {
+      this.filterCirlce = this.circles.filter((circle) => {
+        return circle.image && circle.image.match('https:')
+      })
+      this.searchBox = ''
+    },
+
+    computedCircleByType(type) {
+      this.searchBox = type
+      this.filterCirlce = this.circles.filter((circle) => {
+        return circle.type === type
+      })
+    },
+
+    computedCircleBySearchBox() {
+      this.filterCirlce = filterCircle(this.circles, this.searchBox)
+    }
+  }
+}
+</script>
---
+++ components/organisms/list/CircleImageList.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- ビラ一覧 -->
+  <v-row>
+    <v-col v-for="(circle, key) in circles" :key="key" cols="12" xs="6" sm="4">
+      <v-lazy>
+        <circle-item
+          v-if="circle.id && circle.image && circle.name"
+          :to="`/circles/${circle.id}`"
+          :src="circle.image"
+          :name="circle.shortname || circle.name"
+        />
+      </v-lazy>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+const CircleItem = () => import('@/components/CircleItem.vue')
+
+export default {
+  components: {
+    CircleItem
+  },
+
+  props: {
+    circles: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style></style>
