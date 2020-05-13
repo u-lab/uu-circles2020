fedf860e5eedf2bdf0dbed4d2d9daec12d2f2575
H37kouya <h37kouya@gmail.com>
2020-05-13 20:00:04 +0900

    変更

---
+++ components/organisms/field/MainBackgroundBottom.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-row class="main-background-bottom">
+    <v-col cols="2" sm="3" class="main-background-bottom-l">
+      <div class="text-center">
+        <v-icon color="#fff">mdi-information-variant</v-icon>
+        <p class="mb-0" style="font-size: 10px">重要な<br />お知らせ</p>
+      </div>
+    </v-col>
+
+    <v-col cols="10" sm="9" class="main-background-bottom-r">
+      <p class="mb-0 text-center">
+        新型コロナによって新歓日程に変更が生じる可能性がございます
+      </p>
+    </v-col>
+  </v-row>
+</template>
+
+<style lang="scss" scoped>
+.main-background-bottom {
+  position: absolute;
+  max-width: 500px;
+  bottom: 0;
+  margin: 0 !important;
+
+  &-l {
+    background: grey;
+    color: #fff;
+    padding: 10px !important;
+  }
+
+  &-r {
+    background: rgba(#fff, 0.7);
+    padding: 15px !important;
+
+    p {
+      font-size: 0.9rem;
+    }
+  }
+}
+</style>
---
+++ components/organisms/field/MainBackgroundText.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row style="width: 320px">
+    <v-col cols="4" sm="4" class="d-flex justify-end">
+      <p class="pt-4" style="width: 60px;">
+        <v-img src="/icon-60x60.png" width="60px" style="border-radius: 10px" />
+      </p>
+    </v-col>
+
+    <v-col cols="8" sm="8" md="8">
+      <div class="main-background-content d-flex flex-column">
+        <span>宇都宮大学</span>
+        <span>Utsunomiya</span>
+        <span>University</span>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<style lang="scss" scoped>
+.main-background-content {
+  padding: 10px;
+  border-radius: 10px;
+  color: #fff;
+
+  font-size: 28px;
+  font-weight: bold;
+
+  @include mq(sm) {
+    color: #333;
+    background: rgba(#fff, 0.7);
+  }
+}
+</style>
