83690d69d5aad7c5bbaf489e7a9d20d603bdcf65
H37kouya <h37kouya@gmail.com>
2020-05-19 16:31:48 +0900

    headerの修正

---
+++ components/atoms/text/SiteTitleText.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  props: {
+    element: {
+      type: String,
+      required: true
+    }
+  },
+
+  render(h) {
+    return h(
+      this.element,
+      { class: 'font-weight-bold site-title m-0 white--text' },
+      '宇都宮大学のサークルビラ一覧'
+    )
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.site-title {
+  font-size: 18px;
+  @include mq(sm) {
+    font-size: 30px;
+  }
+}
+</style>
--- components/layouts/Navbar.vue
+++ components/layouts/Navbar.vue
@@ -1,18 +1,26 @@
 <template>
-  <header class="d-flex justify-space-around app-bar mt-4 py-3">
-    <div class="background-container">
-      <div class="text-center background-color">
-        <h1 v-if="$route.path === '/'" class="header-heading m0">
-          宇都宮大学のサークルビラ一覧
-        </h1>
-        <p v-else class="header-heading m0">
-          宇都宮大学のサークルビラ一覧
-        </p>
-        <p class="mb-0 header-subheading">企画・運営 U-lab</p>
-      </div>
-    </div>
+  <header class="app-bar text-center mt-4 py-3">
+    <site-title-text :element="titleElement" />
+
+    <p class="mb-0 header-subheading white--text">企画・運営 U-lab</p>
   </header>
 </template>
+
+<script>
+const SiteTitleText = () => import('@/components/atoms/text/SiteTitleText')
+
+export default {
+  components: {
+    SiteTitleText
+  },
+
+  computed: {
+    titleElement() {
+      return this.$route.path === '/' ? 'h1' : 'p'
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .app-bar {
@@ -23,20 +31,9 @@
 
 .header-subheading {
   font-size: 14px;
-  color: #fff;
 
   @include mq(sm) {
     font-size: 20px;
-  }
-}
-
-.header-heading {
-  font-size: 18px;
-  font-weight: bold;
-  color: #fff;
-  margin-bottom: 0 !important;
-  @include mq(sm) {
-    font-size: 30px;
   }
 }
 </style>
