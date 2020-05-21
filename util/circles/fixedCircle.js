6ace4eb1f8a4a777f851ce8d6efd02af85c35c6c
H37kouya <h37kouya@gmail.com>
2020-05-21 12:59:18 +0900

    fix fixedCircleの修正

--- util/circles/fixedCircle.js
+++ util/circles/fixedCircle.js
@@ -1,9 +1,13 @@
 import { swapArr } from '@/util/array'
 
+/**
+ * 指定のcircleを特定の場所に固定する
+ *
+ * @param {Object[]} circles circleリスト
+ * @param {String} circleId サークルID
+ * @param {Number} fixNum 配列の番号
+ */
 export const fixedCircleById = (circles, circleId, fixNum = 0) => {
-  for (let i = 0; i < circles.length; i++) {
-    if (circles[i].id === circleId) {
-      return swapArr(circles, fixNum, i)
-    }
-  }
+  const idx = circles.findIndex((obj) => obj.id === circleId)
+  return circle !== -1 ? swapArr(circles, fixNum, idx) : circle
 }
