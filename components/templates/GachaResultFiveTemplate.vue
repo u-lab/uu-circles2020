fc7b368ff56ccb77a7a8cb6d3019416156d2243f
H37kouya <h37kouya@gmail.com>
2020-05-21 15:41:15 +0900

    add tempalte

---
+++ components/templates/GachaResultFiveTemplate.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="d-flex justify-center">
+    <v-container>
+      <v-row justify="center">
+        <v-col v-for="gr in circles" :key="gr.id" cols="6" sm="4" md="2">
+          <v-card :to="`/circles/${gr.id}`">
+            <v-img :src="gr.image" :alt="`ガチャ結果-${gr.name}`" />
+
+            <div class="d-flex justify-center">
+              <about-heading :num="2">
+                <span class="font-gacha-result">
+                  {{ gr.shortname }}
+                </span>
+              </about-heading>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+const AboutHeading = () => import('@/components/atoms/heading/AboutHeading')
+
+export default {
+  components: {
+    AboutHeading
+  },
+
+  props: {
+    circles: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.font-gacha-result {
+  font-size: 14px;
+
+  @include mq(sm) {
+    font-size: 16px;
+  }
+}
+</style>
---
+++ components/templates/GachaResultOneTemplate.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="d-flex justify-center">
+    <v-card :to="`/circles/${circle.id}`" class="pa-2" max-width="300px">
+      <v-img :src="circle.image" :alt="`ガチャ結果-${circle.name}`" />
+
+      <div class="d-flex justify-center">
+        <about-heading :num="2">{{ circle.name }}</about-heading>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script>
+const AboutHeading = () => import('@/components/atoms/heading/AboutHeading')
+
+export default {
+  components: {
+    AboutHeading
+  },
+
+  props: {
+    circle: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
