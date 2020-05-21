6b284d5cef7ec0b4bc724b2b5b286fd52c0b7002
H37kouya <h37kouya@gmail.com>
2020-05-22 00:13:28 +0900

    #68 - E2Eテストの導入

---
+++ tests/e2e/error.e2e.js
@@ -0,0 +1,33 @@
+const routes = [
+  '/',
+  '/gacha',
+  '/interviews',
+  '/about/company',
+  '/about/creater',
+  '/circles/u-lab'
+]
+
+describe('すべてのページで500エラーが表示されない', () => {
+  let page
+
+  beforeAll(async () => {
+    jest.setTimeout(50000)
+    page = await browser.newPage()
+  })
+
+  afterAll(async () => {
+    await page.close()
+  })
+
+  for (const route of routes) {
+    it(`エラーが表示されない(${route})`, async () => {
+      await page.goto(`http://localhost:3000${route}`)
+
+      const text = await page.evaluate(
+        () => document.querySelector('h1').textContent
+      )
+      expect(text).not.toBe('An error occurred')
+      expect(text).not.toBe('404 Not Found')
+    })
+  }
+})
