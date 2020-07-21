d81dbf33b6af5b084507d3ec270c0591f9957eeb
H37kouya <h37kouya@gmail.com>
2020-07-21 22:37:56 +0900

    広告追加

---
+++ client/components/molecules/ad/AdEngeeMIx.vue
@@ -0,0 +1,20 @@
+<template>
+  <ad-base href="https://www.instagram.com/engineermix/" :src="logo" />
+</template>
+
+<script>
+import Logo from '@/assets/img/ad/engieer-mix.jpg'
+const AdBase = () => import('@/components/molecules/ad/AdBase')
+
+export default {
+  components: {
+    AdBase,
+  },
+
+  computed: {
+    logo() {
+      return Logo
+    },
+  },
+}
+</script>
