d67231a93dc61983c4a2ad287e29cfc745d190c8
H37kouya <h37kouya@gmail.com>
2020-10-16 23:31:13 +0900

    fix リンク修正

--- client/pages/events/index.vue
+++ client/pages/events/index.vue
@@ -1,11 +1,22 @@
 <template>
   <v-container>
     <about-heading :num="1" class="about-company-list">
-      <span class="haifun">イベント一覧</span>
+      <span class="haifun">新着情報</span>
     </about-heading>
 
     <v-card>
       <v-list>
+        <v-list-item href="https://miyameshi.com" target="_blank">
+          <v-list-item-content>
+            <v-list-item-title>
+              【みやメシ応援隊】U-labが作りました！
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              外部リンクです
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
         <v-list-item to="/events/0613-student-foodbank">
           学生応援きずなセット配布会 (6月13日)
         </v-list-item>
@@ -27,7 +38,11 @@ export default {
   components: {
     AboutHeading,
   },
+
+  head() {
+    return {
+      title: '新着情報',
+    }
+  },
 }
 </script>
-
-<style></style>
