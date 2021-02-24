f865d6d01a12c3d0b24d8eef2c95f6c0573d4c53
H37kouya <h37kouya@gmail.com>
2021-02-24 20:16:46 +0900

    add 広告

---
+++ client/components/molecules/ad/AdCircle.vue
@@ -0,0 +1,23 @@
+<template>
+  <ad-base
+    href="https://uu-circles.com/"
+    :src="logo"
+  />
+</template>
+
+<script>
+import Logo from '@/assets/img/ad/uu-circles2021.png'
+const AdBase = () => import('@/components/molecules/ad/AdBase')
+
+export default {
+  components: {
+    AdBase,
+  },
+
+  computed: {
+    logo() {
+      return Logo
+    },
+  },
+}
+</script>
--- client/components/organisms/block/AdBlock.vue
+++ client/components/organisms/block/AdBlock.vue
@@ -2,35 +2,16 @@
   <div class="text-center pt-1">
     <small>Advertisement</small>
 
-    <components :is="getComponentName" />
+    <AdCircle />
   </div>
 </template>
 
 <script>
-import { randInt } from '@/util/number'
-const AdInterviews = () => import('@/components/molecules/ad/AdInterviews')
-const AdMiyaLunch = () => import('@/components/molecules/ad/AdMiyaLunch')
-const AdEngeeMix = () => import('@/components/molecules/ad/AdEngeeMix')
-
-const rand = () => randInt(8)
+const AdCircle = () => import('@/components/molecules/ad/AdCircle')
 
 export default {
   components: {
-    AdInterviews,
-    AdMiyaLunch,
-    AdEngeeMix,
-  },
-
-  computed: {
-    getComponentName() {
-      const num = rand()
-
-      if (num > 4) {
-        return 'AdMiyaLunch'
-      }
-
-      return 'AdEngeeMix'
-    },
+    AdCircle,
   },
 }
 </script>
